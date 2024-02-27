0c074a2a61f72dd3106a6ce365b05950f91eab14
Francesco Bertolotti <f14.bertolotti@gmail.com>
2024-02-27 11:08:47 +0100

    python tutorial + spark tutorial

--- DSE/Makefike.mk
+++ DSE/Makefike.mk
@@ -3,8 +3,11 @@ venv/bin/activate: ./requirements.txt
 	python3 -m venv venv
 	venv/bin/python3 -m pip install -r requirements.txt
 
+run-python: venv/bin/activate
+	venv/bin/python3 -m jupyter lab ./Python
+
 run-spark: venv/bin/activate
-	venv/bin/python3 -m jupyter lab ./spark
+	venv/bin/python3 -m jupyter lab ./Spark
 
 run-page-rank: venv/bin/activate
 	venv/bin/python3 -m jupyter lab ./PageRank
